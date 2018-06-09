7cfce10b37c3b83e74f6bc40822fc1a12c168dbe
Moshe Avni <moshea@tradair.com>
2018-06-09 21:33:11 +0300

    fix grammar

--- src/data/careerProfile.js
+++ src/data/careerProfile.js
@@ -3,7 +3,7 @@ export const careerProfile = {
     icon: null,
     description: `<p>My passion for computers started when my mom signed me for an HTML course when I was 10   where I built an optical illusion website full of iframes, marquees and cool images 😁. I spent my free time learning about domains, registering for free hosting plans and creating forums, one of them had 1000 unique entrances a day!</p>
 <p>Then, I wanted more and pursued a career path that started from the army service in a technological unit, the rest is listed below so I'll spare that.</p>
-<p>I'm a ighly motivated individual, self-educating ability, constantly learning and developing.
+<p>I'm a highly motivated individual, self-educating ability, constantly learning and developing.
 Punctual, reliable, organized, service oriented and excellent in human relations.
 Very high technical skills, Jack of all trades.</p>
 <p>🇺🇸 Born in USA 🇺🇸</p>
