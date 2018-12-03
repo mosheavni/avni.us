c0959f0b06b27a156647470e2d1f9d4b63b36640
Moshe Avni <moshe.avni@naturalint.com>
2018-12-03 22:37:41 +0200

    Change and add tags

--- src/data/tags.js
+++ src/data/tags.js
@@ -1,9 +1,40 @@
 export const tags = {
     sectionTitle: '',
     icon: null,
-    list: ['Windows Server', 'Linux Server', 'RedHat', 'Cisco Appliances',
-        'CI/CD', 'Jenkins', 'Ansible', 'Bash', 'Python', 'Amazon Web Services', 'Nagios', 'DataDog', 'Consul', 'VMware', 'Maven', 'Artifactory', 'ELK', 'Nginx', 'Apache', 'SQL SERVER', 'MYSQL', 'GitHub', 'GitLab',
-        'Javascript', 'ReactJS', 'Redux', 'NodeJS']
+    list: [
+        'Linux Server',
+        'RedHat',
+        'Ubuntu',
+        'Bash',
+        'Python',
+        'Docker',
+        'Kubernetes',
+        'Swarm',
+        'Amazon Web Services',
+        'CI/CD',
+        'Jenkins',
+        'Ansible',
+        'Nagios',
+        'DataDog',
+        'Consul',
+        'Maven',
+        'Artifactory',
+        'ELK',
+        'NewRelic',
+        'Nginx',
+        'Apache',
+        'MySQL',
+        'Redis',
+        'MongoDB',
+        'GitHub',
+        'GitLab',
+        'VMware',
+        'Windows Server',
+        'Cisco',
+        'Javascript',
+        'ReactJS',
+        'NodeJS'
+    ]
 };
 
 export default tags;
