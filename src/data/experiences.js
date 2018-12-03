28531ea967d5bba367f1063ed5edd504f655fc70
Moshe Avni <moshe.avni@naturalint.com>
2018-12-03 22:45:12 +0200

    add target="_blank"

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -14,10 +14,10 @@ export const experiences = {
             title: 'DevOps Engineer',
             company: 'TradAir',
             description: `<ul>
-<li>Maintaining and creating build (Maven), deployment and update automation jobs with Jenkins and <a href="https://www.ansible.com/">Ansible</a>.</li>
+<li>Maintaining and creating build (Maven), deployment and update automation jobs with Jenkins and <a href="https://www.ansible.com/" target="_blank">Ansible</a>.</li>
 <li>Bash ninja - full *NIX system administration, along with creating cool ease-of-use Python and Bash scripts to make people's life's a little bit easier.</li>
-<li>Working day to day with <a href="https://aws.amazon.com/">Amazon AWS</a> (EC2 [<a href="https://spotinst.com/">SpotInst</a>], RDS, Route53), <a href="https://github.com">Git</a>, <a href="https://jfrog.com/artifactory/">Arfitactory</a>, MySQL servers, <a href="https://www.consul.io/">Consul</a>, <a href="https://www.datadoghq.com/">DataDog</a>, <a href="https://www.vmware.com/products/vcenter-server.html">VMware Vcenter</a></li>
-<li>Creating Javascript (<a href="https://reactjs.org/">React</a> + <a href="https://redux.js.org/">Redux</a>) + <a href="https://nodejs.org/">NodeJS</a> b2b web-UI's for all the teams to further-automate tedious tasks.</li>
+<li>Working day to day with <a href="https://aws.amazon.com/" target="_blank">Amazon AWS</a> (EC2 [<a href="https://spotinst.com/" target="_blank">SpotInst</a>], RDS, Route53), <a href="https://github.com" target="_blank">Git</a>, <a href="https://jfrog.com/artifactory/" target="_blank">Arfitactory</a>, MySQL servers, <a href="https://www.consul.io/" target="_blank">Consul</a>, <a href="https://www.datadoghq.com/" target="_blank">DataDog</a>, <a href="https://www.vmware.com/products/vcenter-server.html" target="_blank">VMware Vcenter</a></li>
+<li>Creating Javascript (<a href="https://reactjs.org/" target="_blank">React</a> + <a href="https://redux.js.org/" target="_blank">Redux</a>) + <a href="https://nodejs.org/" target="_blank">NodeJS</a> b2b web-UI's for all the teams to further-automate tedious tasks.</li>
 </ul>`,
             companyLink: 'https://www.tradair.com',
             companyShortDetail: '',
@@ -26,8 +26,8 @@ export const experiences = {
         {
             title: 'DevOps Engineer',
             company: 'TOYGA',
-            description: `Implementing DevOps technologies: <a href="https://jenkins.io/">Jenkins CI\\CD</a> build\\deploy automations with <a href="https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-reference">MS-Build</a>, <a href="https://www.nagios.org/">Nagios</a> + <a href="https://mathias-kettner.de/check_mk.html">Check_mk</a> monitoring, scripting mainly with Bash.<br /><a href="https://gitlab.com/">Git</a> version management, Log management solution: <a href="https://logz.io/">ELK stack</a>.<br />Linux and Windows system administering of over 50 servers, QA and Production.
-<a href="www.oracle.com/technetwork/database/features/plsql/index.html">PL-SQL</a>, <a href="https://www.mysql.com/">Mysql</a>, <a href="https://www.apache.org/">Apache</a>, <a href="https://www.nginx.com/">Nginx</a> servers managing and administering.`,
+            description: `Implementing DevOps technologies: <a href="https://jenkins.io/" target="_blank">Jenkins CI\\CD</a> build\\deploy automations with <a href="https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-reference" target="_blank">MS-Build</a>, <a href="https://www.nagios.org/" target="_blank">Nagios</a> + <a href="https://mathias-kettner.de/check_mk.html" target="_blank">Check_mk</a> monitoring, scripting mainly with Bash.<br /><a href="https://gitlab.com/" target="_blank">Git</a> version management, Log management solution: <a href="https://logz.io/" target="_blank">ELK stack</a>.<br />Linux and Windows system administering of over 50 servers, QA and Production.
+<a href="www.oracle.com/technetwork/database/features/plsql/index.html" target="_blank">PL-SQL</a>, <a href="https://www.mysql.com/" target="_blank">Mysql</a>, <a href="https://www.apache.org/" target="_blank">Apache</a>, <a href="https://www.nginx.com/" target="_blank">Nginx</a> servers managing and administering.`,
             companyLink: 'www.toyga.co.il/',
             companyShortDetail: '',
             date: 'Jan. - Nov. 2016'
