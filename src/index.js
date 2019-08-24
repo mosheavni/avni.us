572441d52234f06b36c6a4578cb8755dc6007887
Moshe Avni <gatkees@gmail.com>
2019-08-24 16:53:17 +0300

    fix site

--- src/index.js
+++ src/index.js
@@ -2,28 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Data from './data';
-import { unregister } from './registerServiceWorker';
+import { unregister } from './serviceWorker';
 
-// Add this import:
-import { AppContainer } from 'react-hot-loader';
 
-// Wrap the rendering in a function:
-const render = (App) => {
-    ReactDOM.render(
-        // Wrap App inside AppContainer
-        <AppContainer>
-            <App {...Data}/>
-        </AppContainer>,
-        document.getElementById('root')
-    );
-};
+ReactDOM.render(
+    <App {...Data}/>,
+    document.getElementById('root')
+);
 
 unregister();
-render(App);
-
-// Webpack Hot Module Replacement API
-if (module.hot) {
-    module.hot.accept('./App', () => {
-        render(App);
-    });
-}
